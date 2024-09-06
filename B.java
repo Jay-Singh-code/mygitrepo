f8165a0875dd0e8104fa43e3180b344cc748b337
Jay Singh <jay817070@gmail.com>
2024-09-06 23:07:04 +0530

    last commit

--- B.java
+++
@@ -1,8 +0,0 @@
-package com.githubpro;
-
-public class B {
-    public static void main(String[] args) {
-        System.out.println("this is jay");
-        System.out.println("in A.java File");
-    }
-}
